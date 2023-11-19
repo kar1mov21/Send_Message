e7973771bd0a9392848b62d6622287ba49be3d54
HOT-DOG <rightl2053@gmail.com>
2023-11-19 23:56:12 +0500

    update 0.0

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,9 @@ sendBtn.addEventListener('click', sendEmail);
 
 function sendEmail() {
     // Get the form data
-    const to = document.getElementById("to").value;
-    const subject = document.getElementById("subject").value;
-    const message = document.getElementById("message").value;
+    const to = document.querySelector("#to").value;
+    const subject = document.querySelector("#subject").value;
+    const message = document.querySelector("#message").value;
 
     // Send the email using EmailJS
     emailjs.send("service_mgd109v", "template_meb8zzh", {
@@ -20,8 +20,10 @@ function sendEmail() {
         .then(function () {
             result.innerHTML = "Email sent successfully!";
             result.style.opacity = 1;
-        }, function (error) {
+        }, function (err) {
             result.innerHTML = "Email sending failed!";
             result.style.opacity = 1;
+            console.log(err);
+            alert('Email sending failed try again!')
         });
 }
