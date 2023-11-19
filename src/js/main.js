53394d6bf15da9fc0a4a88ffa7aed8d74e8ed567
HOT-DOG <rightl2053@gmail.com>
2023-11-19 23:14:51 +0500

    finish file

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+emailjs.init("j7rMXmo7TXhULGfEI"); // Replace with your EmailJS User ID
+
+const sendBtn = document.querySelector('.send-btn');
+const result = document.querySelector('.result');
+
+sendBtn.addEventListener('click', sendEmail);
+
+function sendEmail() {
+    // Get the form data
+    const to = document.getElementById("to").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    // Send the email using EmailJS
+    emailjs.send("service_mgd109v", "template_meb8zzh", {
+        to_email: to,
+        subject: subject,
+        message: message
+    })
+        .then(function () {
+            result.innerHTML = "Email sent successfully!";
+            result.style.opacity = 1;
+        }, function (error) {
+            result.innerHTML = "Email sending failed!";
+            result.style.opacity = 1;
+        });
+}
